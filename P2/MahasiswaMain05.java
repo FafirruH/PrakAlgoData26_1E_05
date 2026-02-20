3fa3dcecb674cada6c1c29f86a6483acf6966734
FafirruH <fafirruh@gmail.com>
2026-02-20 14:27:57 +0700

    Percobaan3

--- P2/MahasiswaMain05.java
+++ P2/MahasiswaMain05.java
@@ -8,9 +8,14 @@ public class MahasiswaMain05 {
         mhs1.nim   = "2241720171";
         mhs1.kelas = "SI 2J";
         mhs1.ipk   = 3.55;
+
         mhs1.tampilkanInformasi();
         mhs1.ubahKelas("SI 2K");
         mhs1.updateIpk(3.60);
         mhs1.tampilkanInformasi();
+
+        Mahasiswa05 mhs2 = new Mahasiswa05("Annisa Nabila","2141720160",3.25,"TI 2L");
+        mhs2.updateIpk(3.30);
+        mhs2.tampilkanInformasi();
     }
 }
