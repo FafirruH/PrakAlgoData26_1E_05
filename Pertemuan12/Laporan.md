dccc41c52e52788c1bf6f2360796610950bc0b15
FafirruH <fafirruh@gmail.com>
2026-05-07 08:42:51 +0700

    Percobaan1

---
+++ Pertemuan12/Laporan.md
@@ -0,0 +1,9 @@
+# Laporan Praktikum Algoritma dan Struktur Data Jobsheet 10
+
+<h4>Nama : Fafirru Hadzami Syach Mashuri<h4>
+<h4>NIM : 254107020104<h4>
+<h4>Kelas : TI-1E<h4>
+
+# Praktikum Linked List
+## Percobaan 1
+Hasil running percobaan 1
---
+++ Pertemuan12/Mahasiswa00.java
@@ -0,0 +1,22 @@
+package Pertemuan12;
+
+public class Mahasiswa00 {
+    String nim;
+    String nama;
+    String kelas;
+    double ipk;
+
+    Mahasiswa00() {
+    }
+
+    Mahasiswa00(String nm, String name, String kls, double ip) {
+        nim = nm;
+        nama = name;
+        kelas = kls;
+        ipk = ip;
+    }
+
+    void tampilInformasi() {
+        System.out.println(nama+"\t"+nim+"\t"+kelas+"\t"+ipk);
+    }
+}
