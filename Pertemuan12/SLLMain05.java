066947c03c65599806f857d69fb650fdb5a441b6
FafirruH <fafirruh@gmail.com>
2026-05-07 09:37:16 +0700

    percobaan 2

--- Pertemuan12/SLLMain05.java
+++ Pertemuan12/SLLMain05.java
@@ -5,38 +5,63 @@ import java.util.Scanner;
 public class SLLMain05 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        // SingleLinkedList05 sll = new SingleLinkedList05();
+        // System.out.print("Masukkan jumlah data: ");
+        // int jumlah = sc.nextInt();
+        // sc.nextLine();
 
+        // for (int i = 0; i < jumlah; i++) {
+        // System.out.println("Data Mahasiswa ke-" + (i + 1));
+
+        // System.out.print("Nama : ");
+        // String nama = sc.nextLine();
+
+        // System.out.print("NIM : ");
+        // String nim = sc.nextLine();
+
+        // System.out.print("Kelas: ");
+        // String kelas = sc.nextLine();
+
+        // System.out.print("IPK : ");
+        // double ipk = sc.nextDouble();
+        // sc.nextLine();
+
+        // Mahasiswa05 mhs = new Mahasiswa05(nim, nama, kelas, ipk);
+
+        // sll.addLast(mhs);
+        // System.out.println();
+        // }
+
+        // System.out.println("Isi Linked List");
+        // sll.print();
         SingleLinkedList05 sll = new SingleLinkedList05();
+        Mahasiswa05 mhs1 = new Mahasiswa05("24212200", "Alvaro", "1A", 4.0);
+        Mahasiswa05 mhs2 = new Mahasiswa05("23212201", "Bimon", "2B", 3.8);
+        Mahasiswa05 mhs3 = new Mahasiswa05("22212202", "Cimon", "3C", 3.5);
+        Mahasiswa05 mhs4 = new Mahasiswa05("21212203", "Dirga", "4D", 3.6);
 
-        System.out.print("Masukkan jumlah data: ");
-        int jumlah = sc.nextInt();
-        sc.nextLine();
-
-        for (int i = 0; i < jumlah; i++) {
-            System.out.println("Data Mahasiswa ke-" + (i + 1));
-
-            
-            System.out.print("Nama : ");
-            String nama = sc.nextLine();
-            
-            System.out.print("NIM  : ");
-            String nim = sc.nextLine();
-
-            System.out.print("Kelas: ");
-            String kelas = sc.nextLine();
-
-            System.out.print("IPK  : ");
-            double ipk = sc.nextDouble();
-            sc.nextLine();
-
-            Mahasiswa05 mhs = new Mahasiswa05(nim, nama, kelas, ipk);
-
-            sll.addLast(mhs);
-            System.out.println();
-        }
-
-        System.out.println("Isi Linked List");
         sll.print();
+        sll.addFirst(mhs4);
+        sll.print();
+        sll.addLast(mhs1);
+        sll.print();
+        sll.insertAfter("Dirga", mhs3);
+        sll.print();
+        sll.insertAt(2, mhs2);
+        sll.print();
+
+        System.out.println("data index1 : ");
+        sll.getData(1);
+
+        System.out.println("data mahasiswa an Bimon berada pada index : "+sll.indexOf("Bimon"));
+        System.out.println();
+
+        sll.removeFirst();
+        sll.removeLast();
+        sll.print();
+        sll.removeAt(0);
+        sll.print();
+
         sc.close();
     }
 }
--- Pertemuan12/SingleLinkedList05.java
+++ Pertemuan12/SingleLinkedList05.java
@@ -77,5 +77,87 @@ public class SingleLinkedList05 {
             }
         }
     }
-    
+
+    public void getData(int index) {
+        Node05 tmp = head;
+        for (int i = 0; i < index; i++) {
+            tmp = tmp.next;
+        }
+        tmp.data.tampilInformasi();
+    }
+
+    public int indexOf(String key) {
+        Node05 tmp = head;
+        int index = 0;
+        while (tmp != null && !tmp.data.nama.equalsIgnoreCase(key)) {
+            tmp = tmp.next;
+            index++;
+        }
+        if (tmp == null) {
+            return -1;
+        } else {
+            return index;
+        }
+    }
+
+    public void removeFirst() {
+        if (isEmpty()) {
+            System.out.println("Linked list masih kosong, tidak dapat dihapus!");
+        } else if (head == tail) {
+            head = tail = null;
+        } else {
+            head = head.next;
+        }
+    }
+
+    public void removeLast() {
+        if (isEmpty()) {
+            System.out.println("Linked list masih kosong, tidak dapat dihapus!");
+        } else if (head == tail) {
+            head = tail = null;
+        } else {
+            Node05 temp = head;
+            while (temp.next != tail) {
+                temp = temp.next;
+            }
+            temp.next = null;
+            tail = temp;
+        }
+    }
+
+    public void remove(String key) {
+        if (isEmpty()) {
+            System.out.println("Linked list masih kosong, tidak dapat dihapus!");
+        } else {
+            Node05 temp = head;
+            while (temp != null) {
+                if ((temp.data.nama.equals(key)) && (temp == head)) {
+                    this.removeFirst();
+                    break;
+                } else if (temp.next.data.nama.equals(key)) {
+                    temp.next = temp.next.next;
+                    if (temp.next == null) {
+                        tail = temp;
+                    }
+                    break;
+                }
+                temp = temp.next;
+            }
+        }
+    }
+
+    public void removeAt(int index) {
+        if (index == 0) {
+            removeFirst();
+        } else {
+            Node05 temp = head;
+            for (int i = 0; i < index - 1; i++) {
+                temp = temp.next;
+            }
+            temp.next = temp.next.next;
+            if (temp.next == null) {
+                tail = temp;
+            }
+        }
+    }
 }
