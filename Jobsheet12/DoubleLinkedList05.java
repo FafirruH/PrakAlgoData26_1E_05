a08026f7dc704cde09e74e16c4b7b5f3f66ff03c
FafirruH <fafirruh@gmail.com>
2026-05-21 18:37:17 +0700

    modifikasi percobaan 2

--- Jobsheet12/DoubleLinkedList05.java
+++ Jobsheet12/DoubleLinkedList05.java
@@ -1,129 +1,129 @@
 package Jobsheet12;
 
 public class DoubleLinkedList05 {
-    Node05 head;
-    Node05 tail;
+  Node05 head;
+  Node05 tail;
 
-    public DoubleLinkedList05() {
-        head = null;
-        tail = null;
+  public DoubleLinkedList05() {
+    head = null;
+    tail = null;
+  }
+
+  boolean isEmpty() {
+    return head == null;
+  }
+
+  public void addFirst(Mahasiswa05 data) {
+    Node05 newNode = new Node05(data);
+
+    if (isEmpty()) {
+      head = tail = newNode;
+    } else {
+      newNode.next = head;
+      head.prev = newNode;
+      head = newNode;
+    }
+  }
+
+  public void addLast(Mahasiswa05 data) {
+    Node05 newNode = new Node05(data);
+
+    if (isEmpty()) {
+      head = tail = newNode;
+    } else {
+      tail.next = newNode;
+      newNode.prev = tail;
+      tail = newNode;
+    }
+  }
+
+  public void insertAfter(String keyNim, Mahasiswa05 data) {
+    Node05 current = head;
+
+    while (current != null && !current.data.nim.equals(keyNim)) {
+      current = current.next;
     }
 
-    boolean isEmpty() {
-        return head == null;
+    if (current == null) {
+      System.out.println("Data dengan NIM " + keyNim + " tidak ditemukan.");
+      return;
     }
 
-    public void addFirst(Mahasiswa05 data) {
-        Node05 newNode = new Node05(data);
+    Node05 newNode = new Node05(data);
 
-        if (isEmpty()) {
-            head = tail = newNode;
-        } else {
-            newNode.next = head;
-            head.prev = newNode;
-            head = newNode;
-        }
+    if (current == tail) {
+      newNode.prev = current;
+      current.next = newNode;
+      tail = newNode;
+    } else {
+      newNode.prev = current;
+      newNode.next = current.next;
+      current.next.prev = newNode;
+      current.next = newNode;
     }
 
-    public void addLast(Mahasiswa05 data) {
-        Node05 newNode = new Node05(data);
+    System.out.println("Data berhasil disisipkan setelah NIM " + keyNim);
+  }
 
-        if (isEmpty()) {
-            head = tail = newNode;
-        } else {
-            tail.next = newNode;
-            newNode.prev = tail;
-            tail = newNode;
-        }
+  public void print() {
+    if (isEmpty()) {
+      System.out.println("Linked List masih kosong");
+      return;
     }
 
-    public void insertAfter(String keyNim, Mahasiswa05 data) {
-        Node05 current = head;
+    Node05 current = head;
 
-        while (current != null && !current.data.nim.equals(keyNim)) {
-            current = current.next;
-        }
+    while (current != null) {
+      current.data.tampil();
+      System.out.println("-------------------");
+      current = current.next;
+    }
+  }
 
-        if (current == null) {
-            System.out.println("Data dengan NIM " + keyNim + " tidak ditemukan.");
-            return;
-        }
-
-        Node05 newNode = new Node05(data);
-
-        if (current == tail) {
-            newNode.prev = current;
-            current.next = newNode;
-            tail = newNode;
-        } else {
-            newNode.prev = current;
-            newNode.next = current.next;
-            current.next.prev = newNode;
-            current.next = newNode;
-        }
-
-        System.out.println("Data berhasil disisipkan setelah NIM " + keyNim);
+  public void removeFirst() {
+    if (isEmpty()) {
+      System.out.println("Linked List kosong");
+      return;
     }
 
-    public void print() {
-        if (isEmpty()) {
-            System.out.println("Linked List masih kosong");
-            return;
-        }
+    System.out.println("Data " + head.data.nama + " berhasil dihapus");
 
-        Node05 current = head;
+    if (head == tail) {
+      head = tail = null;
+    } else {
+      head = head.next;
+      head.prev = null;
+    }
+  }
 
-        while (current != null) {
-            current.data.tampil();
-            System.out.println("-------------------");
-            current = current.next;
-        }
+  public void removeLast() {
+    if (isEmpty()) {
+      System.out.println("Linked List kosong");
+      return;
     }
 
-    public void removeFirst() {
-        if (isEmpty()) {
-            System.out.println("Linked List kosong");
-            return;
-        }
+    System.out.println("Data " + tail.data.nama + " berhasil dihapus");
 
-        System.out.println("Data " + head.data.nama + " berhasil dihapus");
+    if (head == tail) {
+      head = tail = null;
+    } else {
+      tail = tail.prev;
+      tail.next = null;
+    }
+  }
 
-        if (head == tail) {
-            head = tail = null;
-        } else {
-            head = head.next;
-            head.prev = null;
-        }
+  public void printReverse() {
+    if (isEmpty()) {
+      System.out.println("Linked List masih kosong");
+      return;
     }
 
-    public void removeLast() {
-        if (isEmpty()) {
-            System.out.println("Linked List kosong");
-            return;
-        }
+    Node05 current = tail;
 
-        System.out.println("Data " + tail.data.nama + " berhasil dihapus");
-
-        if (head == tail) {
-            head = tail = null;
-        } else {
-            tail = tail.prev;
-            tail.next = null;
-        }
+    while (current != null) {
+      current.data.tampil();
+      System.out.println("-------------------");
+      current = current.prev;
     }
-
-    public void printReverse() {
-        if (isEmpty()) {
-            System.out.println("Linked List masih kosong");
-            return;
-        }
-
-        Node05 current = tail;
-
-        while (current != null) {
-            current.data.tampil();
-            System.out.println("-------------------");
-            current = current.prev;
-        }
-    }
+  }
 }
